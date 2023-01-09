2a6cdc900fb353dfa1e319a27ee3109543037359
Miguel Mota <hello@miguelmota.com>
2023-01-09 10:24:53 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hop Galxe Optimism Quest Calculator
+
+> A simple calculator to track the [Hop](https://app.hop.exchange/) Protocol Optimism [Quest](https://app.optimism.io/quests/hop-provide-liquidity) progress for [Galxe](https://galxe.com/Optimism/campaign/GCXDiUtmVW) NFT.
+
+[https://hop-op-quest-calculator.netlify.app](https://hop-op-quest-calculator.netlify.app)
+
+## License
+
+MIT
---
+++ main.js
@@ -0,0 +1,39 @@
+const $token = document.querySelector('#token')
+const $amountDeposited = document.querySelector('#amountDeposited')
+const $form = document.querySelector('#form')
+const $output = document.querySelector('#output')
+
+const SECONDS_IN_DAY = 86400
+const STABLE_TOKEN_DAYS = 18250
+const ETH_TOKEN_DAYS = 12 // STABLE_TOKEN_DAYS / 1500
+const SNX_TOKEN_DAYS = 7300 // STABLE_TOKEN_DAYS / 2.50
+
+function getTokenDays (token) {
+  if (token === 'ETH') {
+    return ETH_TOKEN_DAYS
+  } else if (token === 'SNX') {
+    return SNX_TOKEN_DAYS
+  }
+
+  return STABLE_TOKEN_DAYS
+}
+
+function calcNumDaysNeeded (token, amount) {
+  return getTokenDays(token) / amount
+}
+
+// example calculate token balance
+// function calcRate(token, amount) {
+//  const rate = STABLE_TOKEN_DAYS/getTokenDays(token)
+//  return rate * amount
+// }
+// const totalBalance = 1520.83334 + calcRate(11)
+
+$form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const token = $token.value
+  const amount = Number($amountDeposited.value)
+
+  const days = calcNumDaysNeeded(token, amount)
+  $output.innerText = `${days.toFixed(2)} ${days === 1 ? 'day' : 'days'}`
+})
